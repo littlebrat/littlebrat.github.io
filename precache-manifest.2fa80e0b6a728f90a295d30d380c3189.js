b91ad3dfc31b696182b2a16533d9fb3a74aec1fe
littlebrat <gugas93@hotmail.com>
2019-08-02 14:10:59 +0200

    Updates

---
+++ precache-manifest.2fa80e0b6a728f90a295d30d380c3189.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff00d1999761adbc1b51193d3ee016a",
+    "url": "/personal/index.html"
+  },
+  {
+    "revision": "0a95f44817da24e3dbc4",
+    "url": "/personal/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/personal/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "0a95f44817da24e3dbc4",
+    "url": "/personal/static/js/main.9571ca9a.chunk.js"
+  },
+  {
+    "revision": "28e4a844449b63ec867d",
+    "url": "/personal/static/js/runtime~main.1ce33e60.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal/static/media/logo.5d5d9eef.svg"
+  }
+]);
